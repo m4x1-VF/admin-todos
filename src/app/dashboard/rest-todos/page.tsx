69acf13dabfa4ff396aaedc00e75a0aff082f497
Maximiliano Vilariño <maxi.vilarino@gmail.com>
2024-03-14 09:25:09 +0100

    Add REST API integration and display todos in a grid

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,7 +1,16 @@
-export default function RestTodosPage() {
+import prisma from "@/lib/prisma";
+import { TodosGrid } from "@/todos";
+
+export const metadata = {
+  title: "List of Todos",
+  description: "SEO Title",
+};
+
+export default async function RestTodosPage() {
+  const todos = await prisma.todo.findMany({ orderBy: { description: "asc" } });
   return (
     <div>
-      <h1 className="text-2xl">RestTodo Page</h1>
+      <TodosGrid todos={todos} />
     </div>
   );
 }
